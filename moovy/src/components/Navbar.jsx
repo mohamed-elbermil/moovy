54bfc09f4d4e7b324a1b0da9e2334b771f2925cc
MohamedELBERMIL69780 <mohamed-du-69780@hotmail.fr>
2025-07-20 19:07:09 +0200

    création navbar

--- moovy/src/components/Navbar.jsx
+++ moovy/src/components/Navbar.jsx
@@ -1,26 +1,24 @@
 import styles from "./Navbar.module.css";
 import Link from "next/link";
-import Image from next/image /* Permet un chargement plus rapide et responsive */
-import Logo from "../assets/images/logo.svg"
+import Image from "next/image"; /* Permet un chargement plus rapide et responsive */
+import Logo from "../assets/images/logo.svg";
 
 export default function Navbar() {
   return (
-    <nav>
-        <Image 
-            src={Logo}
-            alt="Logo Moovy"  
-            width={150}   
-            height={150 }   
-        />
-        <div>
-            <Link href="/">Accueil</Link>
-            <Link href="/">Films</Link>
-            <Link href="/">Séries</Link>
-        </div>
-        <div>
-            <input type="search" name="" id="" />
-            <button>Connexion</button>
-        </div>
+    <nav className={styles.navbar}>
+      <Link href="/">
+        <Image src={Logo} alt="Logo Moovy" width={150} height={34} />
+      </Link>
+
+      <div className="flex flex-row items-center h-full gap-5 md:gap-10 hidden sm:flex">
+        <Link href="/">Accueil</Link>
+        <Link href="/">Films</Link>
+        <Link href="/">Séries</Link>
+      </div>
+      <div>
+        <input type="search" name="" id="" />
+        <button>Connexion</button>
+      </div>
     </nav>
   );
 }
