8501d34282a47b211971d453d9b384926f75da1c
MohamedELBERMIL69780 <mohamed-du-69780@hotmail.fr>
2025-07-23 19:08:22 +0200

    création de la bannière

--- moovy/src/app/layout.tsx
+++ moovy/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import "../styles/reset.css";
 import Navbar from "@/components/Navbar/Navbar";
-
+import Header from "@/components/Header/Header"
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -10,6 +10,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Navbar></Navbar>
+        <Header></Header>
         {children}
       </body>
     </html>
---
+++ moovy/src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header() {
+    return(
+        <div>
+            <h1>Lorem ipsum</h1>
+            <div className="categorieList">
+                <span>12 +</span>
+                <p>Action</p> 
+            </div>
+            <p>Ceci est une description de film Dolor Lorem ipsum Dolor Lorem ipsum Dolor Lorem ipsum Dolor Lorem ipsum Dolor Lorem ipsum Dolor Lorem ipsum Dolor Lorem ipsum Dolor </p>
+            <button>Lecture</button>
+        </div>
+        
+    )
+}
