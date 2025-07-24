dc752f239d129fb0a4a82184aa0e66d5e13bee8d
MohamedELBERMIL69780 <mohamed-du-69780@hotmail.fr>
2025-07-24 15:50:41 +0200

    ajout de l'api TMDB pour récupérer les films (poster, description, titre, categorie

--- moovy/src/app/page.tsx
+++ moovy/src/app/page.tsx
@@ -1,13 +1,24 @@
-import ContentCarousel from "../components/ContentCarousel/ContentCarousel"
+import { access } from "fs";
+import ContentCarousel from "../components/ContentCarousel/ContentCarousel";
 
-const moviesItems = [
-  {title:'Stranger things', imageUrl:'https://static.posters.cz/image/1300/129581.jpg', description: 'lorem ipsum dolor rvevevee', style:"Série"}
-]
+export default async function Home() {
+  const API_KEY = process.env.TMDB_API_KEY;
+  console.log("API_KEY =", API_KEY);
+  const res = await fetch(
+    `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=fr&sort_by=popularity.desc`,
+    { cache: 'no-store' }
+  );
 
-export default function Home() {
+  if (!res.ok) {
+    console.error("Erreur fetch TMDb :", res.status);
+    return <main>Erreur lors du chargement des films</main>;
+  }
+
+  const data = await res.json();
+
   return (
     <main>
-      <ContentCarousel items={moviesItems} />
+      <ContentCarousel movies={data.results || []} />
     </main>
   );
 }
--- moovy/src/components/ContentCarousel/ContentCarousel.jsx
+++ moovy/src/components/ContentCarousel/ContentCarousel.jsx
@@ -1,17 +1,24 @@
 import styles from "../ContentCarousel/ContentCarousel.module.css"
 
-const ContentCarousel = ({items}) => {
+const ContentCarousel = ({movies}) => {
  return(
     <section>
         <div className={styles.carousel}>
-            {items.map((item, index) => (
-                <div key={index} className={styles.movie}>
-                    <span>{item.style}</span>
-                    <img src={item.imageUrl} alt={item.title} />
+            {movies.map((movie, index) => (
+                <div key={movie.id || index} className={styles.movie}>
+                    <span>{movie.genre_ids?.join(',')}</span>
+                    {movie.poster_path ? (
+                        <img 
+                        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                        alt={movie.title}
+                        className={styles.poster}
+                        />
+                    ) : (
+                        <div>Pas d'affiche</div>
+                    )}
                     <div className={styles.activeCard}>
-                    <p>{item.title}</p>
-                    <p>{item.description}</p>
-
+                        <p>{movie.title}</p>
+                        <p>{movie.overview}</p>
                     </div>
                 </div>
             ))}
