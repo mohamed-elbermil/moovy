9082c58d138db3c2be59e6480c3ea96d954f00c9
mohamed-elbermil <mohamed-du-69780@hotmail.fr>
2025-08-10 08:56:59 +0200

    films filtrés

--- moovy/src/app/page.tsx
+++ moovy/src/app/page.tsx
@@ -3,29 +3,56 @@ import ContentCarousel from "../components/ContentCarousel/ContentCarousel";
 import FollowUsBar from "../components/FollowUsBar/FollowUsBar"
 import "../styles/variables.css"
 import "../styles/base.css"
+import genreMap from "../data/genreMap"
+
+
 
 export default async function Home() {
+  
+  
   const API_KEY = process.env.TMDB_API_KEY;
   console.log("API_KEY =", API_KEY);
   const res = await fetch(
     `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=fr&sort_by=popularity.desc`,
     { cache: 'no-store' }
   );
-
+  
   if (!res.ok) {
     console.error("Erreur fetch TMDb :", res.status);
     return <main>Erreur lors du chargement des films</main>;
   }
-
+  
   const data = await res.json();
+  const actionGenreId = 28; 
+  const horrorGenreId = 27;   
+  const fantasticGenreId = 14;  
+  
+  // Filtrer les films action et horreur
+  const actionMovies = data.results.filter(movie => movie.genre_ids.includes(actionGenreId));
+  const horrorMovies = data.results.filter(movie => movie.genre_ids.includes(horrorGenreId));
+  const fantasticMovies = data.results.filter(movie => movie.genre_ids.includes(fantasticGenreId));
 
   return (
     <main>
-      <ContentCarousel movies={data.results || []} />
-      <FollowUsBar/>
-      <ContentCarousel movies={data.results || []} />
-      <ContentCarousel movies={data.results || []} />
-      <ContentCarousel movies={data.results || []} />
+      <ContentCarousel 
+        movies={actionMovies} 
+        title="Action Non-Stop" 
+        subtitle="Les sensations fortes à couper le souffle"
+      />
+
+      <FollowUsBar />
+
+      <ContentCarousel 
+        movies={horrorMovies} 
+        title="Frissons & Terreur" 
+        subtitle="Osez plonger dans l’horreur ultime"
+      />
+
+      <ContentCarousel 
+        movies={fantasticMovies}
+        title="Univers Fantastiques" 
+        subtitle="Des mondes imaginaires qui vous captivent"
+      />
     </main>
   );
 }
--- moovy/src/components/ContentCarousel/ContentCarousel.jsx
+++ moovy/src/components/ContentCarousel/ContentCarousel.jsx
@@ -17,63 +17,65 @@ const openTrailer = async (movieId) => {
     }
   };
 
-const ContentCarousel = ({movies}) => {
+  const ContentCarousel = ({movies, title, subtitle}) => {
     const moviesRef = useRef(null);
+  
     const scrollLeft = () => {
-        moviesRef.current.scrollBy({left: -500, behavior: "smooth"});
+      moviesRef.current.scrollBy({left: -500, behavior: "smooth"});
     }
     const scrollRight = () => {
-        moviesRef.current.scrollBy({left: 500, behavior: "smooth"});
+      moviesRef.current.scrollBy({left: 500, behavior: "smooth"});
     }
+  
     return (
-        <section>
-            <div className={styles.carousel}>
-                <div className={styles.titleList}>
-                    <span>Les dernières nouveauté </span>
-                    <h1>Sortie cette année</h1>
+      <section>
+        <div className={styles.carousel}>
+          <div className={styles.titleList}>
+            <span>{subtitle}</span>
+            <h1>{title}</h1>
+          </div>
+          <Btn variant="arrow" className={styles.arrowLeft} onClick={scrollLeft}></Btn>
+          <Btn variant="arrow" className={styles.arrowRight} onClick={scrollRight}></Btn>
+          <div className={styles.container} >
+            <div className={styles.movies} ref={moviesRef}>
+              {movies.map((movie, index) => (
+                <div key={movie.id || index} className={styles.movie}>
+                  {movie.poster_path ? (
+                    <img 
+                      src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                      alt={movie.title}
+                      className={styles.poster}
+                    />
+                  ) : (
+                    <div>Pas d'affiche pour ce film 🙄</div>
+                  )}
+                  <div className={styles.activeCard}>
+                    <span className={styles.genres}>
+                      {movie.genre_ids
+                        ?.map(id => genreMap[id])
+                        .filter(Boolean)
+                        .slice(0,2)
+                        .map((genre, index) => (
+                          <div key={index} className={styles.genreItem}>
+                            {genre}
+                          </div>
+                      ))}
+                    </span>
+                    <p className={styles.title}>{movie.title}</p>                  
+                    <p className={styles.overview}>
+                      {movie.overview.length > 200
+                        ? movie.overview.slice(0,200) + " ..."
+                        : movie.overview}
+                    </p>
+                    <Btn className={styles.btnTrailer} onClick={() => openTrailer(movie.id)}>Voir Trailer</Btn>
+                  </div>
                 </div>
-                <Btn variant="arrow" className={styles.arrowLeft} onClick={scrollLeft}></Btn>
-                <Btn variant="arrow" className={styles.arrowRight} onClick={scrollRight}></Btn>
-                <div className={styles.container} >
-                    <div className={styles.movies} ref={moviesRef}>
-                        {movies.map((movie, index) => (
-                            <div key={movie.id || index} className={styles.movie}>
-                                {movie.poster_path ? (
-                                    <img 
-                                        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                                        alt={movie.title}
-                                        className={styles.poster}
-                                    />
-                                ) : (
-                                    <div>Pas d'affiche pour ce film 🙄</div>
-                                )}
-                                <div className={styles.activeCard}>
-                                    <span className={styles.genres}>
-                                    {movie.genre_ids
-                                        ?.map(id => genreMap[id])
-                                        .filter(Boolean)
-                                        .slice(0,2)
-                                        .map((genre, index) => (
-                                        <div key={index} className={styles.genreItem}>
-                                            {genre}
-                                        </div>
-                                        ))}
-                                    </span>
-                                    <p className={styles.title}>{movie.title}</p>                  
-                                    <p className={styles.overview}>
-                                        {movie.overview.length > 200
-                                            ? movie.overview.slice(0,200) + " ..."
-                                            : movie.overview}
-                                    </p>
-                                    <Btn className={styles.btnTrailer} onClick={() => openTrailer(movie.id)}>Voir Trailer</Btn>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                </div>
+              ))}
             </div>
-        </section>
-    )   
-}
-
+          </div>
+        </div>
+      </section>
+    )
+  }
+  
 export default ContentCarousel;
